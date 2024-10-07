c1803ce578457623fd35bead746afe6454c57b4f
StandFast50 <75770927+StandFast50@users.noreply.github.com>
2024-10-06 23:04:29 -0700

    0.3
    It seems to be working. We'll run a few more test before going to 1.0

--- main.py
+++ main.py
@@ -45,17 +45,25 @@ for i in range(n):
 y_subintervals = [0] * (n)
 for i in range(n):
     x_value = x_midpoints[i]
-    x1 = x_subintervals[i]
-    x2 = x_subintervals[i+1]
-    y1 = y[i]
-    y2 = y[i + 1]
-    calculation = y1 + ((x_value - x1) / (x2 - x1)) * (y2 - y1)
-    y_subintervals[i] = calculation
-    print(f"x: {x_value}")
-    print(f"x1: {x1}")
-    print(f"x2: {x2}")
-    print(f"y1: {y1}")
-    print(f"y2: {y2}")
-    print(f"calculation { calculation}")
-    print("**************")
-                                
+
+    for j in range(len(x) - 1):
+        if x[j] <= x_value <= x[j + 1]:
+            x1 = x[j]
+            x2 = x[j+1]
+            y1 = y[j]
+            y2 = y[j + 1]
+            calculation = y1 + ((x_value - x1) / (x2 - x1)) * (y2 - y1)
+            y_subintervals[i] = calculation
+            print(f"x: {x_value}")
+            print(f"x1: {x1}")
+            print(f"x2: {x2}")
+            print(f"y1: {y1}")
+            print(f"y2: {y2}")
+            print(f"calculation { calculation}")
+            print("**************")
+            break
+
+area = 0                               
+for i in range(len(y_subintervals)):
+    area += (y_subintervals[i] * dx)
+print(f"area: {area}")
